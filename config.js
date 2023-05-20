808b7c3bbb6c7371941c0e4436b0e660cbb6077a
Dang Duc Ba <badang0509@gmail.com>
2023-05-20 21:36:43 +0700

    happy_birday project

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "Ly";
+const giftUrl = "http://nodemy.vn";
+const eventName = "Chúc Mừng Sinh Nhật";
+const titleCard = "Tặng Nguyễn Thị Nhật Ly";
+const contentCard =
+  "Chúc Ly sinh nhật vui vẻ, tràn ngập niềm vui và những nụ cười, tuổi mới xinh lại thêm xinh, duyên lại càng thêm duyên và yêu đời nè!. Mong điều đẹp nhất sẽ đến với Ly trong hôm nay và cả những ngày sau";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "nhat_ly.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
